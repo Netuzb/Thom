37cab4c824ceeed7e01e687669fa7ccd9b986cad
erkinovtemur70@gmail.com <erkinovtemur70@gmail.com>
2022-10-03 22:25:45 +0500

    adding: purge, del

--- thom/modules/thom_settings.py
+++ thom/modules/thom_settings.py
@@ -409,13 +409,81 @@ class ThomSettingsMod(loader.Module):
 
     @loader.command(ru_doc="Очистить логи")
     async def clearlogs(self, message: Message):
-        """Clear logs"""
+        """Musorlani tozalash xullas"""
         for handler in logging.getLogger().handlers:
             handler.buffer = []
             handler.handledbuffer = []
             handler.tg_buff = ""
 
         await utils.answer(message, self.strings("logs_cleared"))
+				
+				@loader.command(ru_doc="Быстро удаление")
+    async def purge(self, message):
+        """Barcha habarlarni oʻchirish"""
+        if not message.is_reply:
+            await utils.answer(message, self.strings("from_where", message))
+            return
+        from_users = set()
+        args = utils.get_args(message)
+        for arg in args:
+            try:
+                try:
+                    arg = int(arg)
+                except:
+                    pass
+                entity = await message.client.get_entity(arg)
+                if isinstance(entity, telethon.tl.types.User):
+                    from_users.add(entity.id)
+            except ValueError:
+                pass
+        msgs = []
+        from_ids = set()
+        if await message.client.is_bot():
+            if not message.is_channel:
+                await utils.answer(message, self.strings("not_supergroup_bot", message))
+                return
+            for msg in range(message.reply_to_msg_id, message.id + 1):
+                msgs.append(msg)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        else:
+            async for msg in message.client.iter_messages(
+                entity=message.to_id, min_id=message.reply_to_msg_id - 1, reverse=True
+            ):
+                if from_users and msg.sender_id not in from_users:
+                    continue
+                msgs.append(msg.id)
+                from_ids.add(msg.sender_id)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        if msgs:
+            logger.debug(msgs)
+            await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log("purge", group=message.to_id, affected_uids=from_ids)
+				
+				@loader.command(ru_doc="Удаление сообщения")
+    async def del(self, message):
+        """Habarni oʻchirish"""
+        msgs = [message.id]
+        if not message.is_reply:
+            if await message.client.is_bot():
+                await utils.answer(message, self.strings("delete_what", message))
+                return
+            msg = await message.client.iter_messages(
+                message.to_id, 1, max_id=message.id
+            ).__anext__()
+        else:
+            msg = await message.get_reply_message()
+        msgs.append(msg.id)
+        logger.debug(msgs)
+        await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log(
+            "delete", group=message.to_id, affected_uids=[msg.sender_id]
+        )
 
     @loader.command(ru_doc="Включить NoNick для определенного пользователя")
     async def nonickuser(self, message: Message):
